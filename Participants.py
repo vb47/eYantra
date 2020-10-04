f60ffcfc745cf2b2f32146116bdcad39cf7a4dcc
YJ-928 <ymbj28112000@gmail.com>
2020-10-04 16:51:53 +0530

    Change by yash

--- Participants.py
+++ Participants.py
@@ -3,7 +3,7 @@ def participants():
 	print("Vaibhav Alur")
 	print("Abhishek Kolvekar")
 	print("Abhishek Anavekar")
-	print("Yash Joshi")
+	print("Yash B. Joshi")
 
 print("eYantra 2020-21 team #161: ")
 participants()
